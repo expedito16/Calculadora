ea30753f1e5e0bd092077646b1e7f77dc4cdd8f2
Expedito Neto <expedito.neto@osf.digital>
2021-04-05 21:28:15 -0300

    criando service para realizar as operações na calculadora ao receber os valores

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,34 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /*Define as constantes utilizadas
+  para identificar as operações de cálculo*/
+
+  static readonly soma: string = '+';
+  static readonly subtracao: string = '-';
+  static readonly multiplicacao: string = '*';
+  static readonly divisao: string = '/';
+
   constructor() { }
+
+  calcular(valor1: number, valor2: number, operacao: string): any {
+    let resultado: number; //Armazena o resultado da operação.
+
+    switch (operacao) {
+      case CalculadoraService.soma:
+        resultado = valor1 + valor2;
+      break;
+      case CalculadoraService.subtracao:
+        resultado = valor1 - valor2;
+      break;
+      case CalculadoraService.multiplicacao:
+        resultado = valor1 * valor2;
+      break;
+      case CalculadoraService.divisao:
+        resultado = valor1 * valor2;
+      break;
+      default:
+        resultado = 0;
+    }
+  }
 }
